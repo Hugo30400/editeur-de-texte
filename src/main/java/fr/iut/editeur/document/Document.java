079e169c8c422c474a4999fc1d2ad8aef96b3f79
Mackbook Hugo <hugo.faus@etu.umontpellier.fr>
2024-09-03 19:31:59 +0200

    Commande majuscules

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -26,6 +26,11 @@ public class Document {
         texte = partieGauche + remplacement + partieDroite;
     }
 
+    public void majuscules(int debut, int fin) {
+        String enMajuscule = texte.substring(debut, fin + 1);
+        remplacer(debut, fin, enMajuscule.toUpperCase());
+    }
+
     @Override
     public String toString() {
         return this.texte;
