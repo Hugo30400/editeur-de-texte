7d30d25774c58373d1489e149e0960824f869b60
Mackbook Hugo <hugo.faus@etu.umontpellier.fr>
2024-09-06 09:20:10 +0200

    Création de la class CommandeEffacer.java

---
+++ src/main/java/fr/iut/editeur/commande/CommandeEffacer.java
@@ -0,0 +1,11 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeEffacer extends CommandeDocument {
+
+    public CommandeEffacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+}
