4e13ad76d13ab16b57a03ca18dc131e20ea8e2b2
Mackbook Hugo <hugo.faus@etu.umontpellier.fr>
2024-09-06 09:26:58 +0200

    CommandeEffacer

--- src/main/java/fr/iut/editeur/commande/CommandeEffacer.java
+++ src/main/java/fr/iut/editeur/commande/CommandeEffacer.java
@@ -8,4 +8,12 @@ public class CommandeEffacer extends CommandeDocument {
         super(document, parameters);
     }
 
+    @Override
+    public void executer() {
+        if(parametersCheck(3, "Format attendu : effacer;Debut;Fin")) return;
+        if(parametersNumbersCheck(parameters[1], parameters[2])) return;
+
+        this.document.effacer(Integer.parseInt(parameters[1]), Integer.parseInt(parameters[2]));
+        super.executer();
+    }
 }
