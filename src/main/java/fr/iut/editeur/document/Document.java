e8c4ad9e4b0a5a887cd82cbeb6d51c07f075fe60
Mackbook Hugo <hugo.faus@etu.umontpellier.fr>
2024-09-11 09:53:48 +0200

    docs : Document.java

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -22,21 +22,41 @@ public class Document {
         this.texte = texte;
     }
 
+    /**
+     * Ajouter à this le texte placé en paramètre
+     * @param texte contient le texte à ajouter
+     */
     public void ajouter(String texte) {
         this.texte += texte;
     }
 
+    /**
+     * Remplace la section choisie de @this par @remplacement
+     * @param debut index de début de la section à remplacer
+     * @param fin index de fin de la section à remplacer
+     * @param remplacement texte de la section remplacée
+     */
     public void remplacer(int debut, int fin, String remplacement) {
         String partieGauche = texte.substring(0, debut);
         String partieDroite = texte.substring(fin + 1);
         texte = partieGauche + remplacement + partieDroite;
     }
 
+    /**
+     * Met en majuscule la section choisie de @this
+     * @param debut index de début de la section à mettre en majuscule
+     * @param fin index de fin de la section à mettre en majuscule
+     */
     public void majuscules(int debut, int fin) {
         String enMajuscule = texte.substring(debut, fin + 1);
         remplacer(debut, fin, enMajuscule.toUpperCase());
     }
 
+    /**
+     * Efface la section choisie de this
+     * @param debut index de début de la section à effacer
+     * @param fin index de fin de la section à effacer
+     */
     public void effacer(int debut, int fin) {
         remplacer(debut, fin, "");
     }
